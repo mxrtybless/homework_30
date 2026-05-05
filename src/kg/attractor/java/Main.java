25ccc745f0bc628476f2e2643f4dcefec779db91
Mxrty <ruam1nske@gmail.com>
2026-05-05 14:52:56 +0300

    mplement order total calculation

---
+++ src/kg/attractor/java/Main.java
@@ -0,0 +1,19 @@
+package kg.attractor.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // это для занятия
+        var movieCollection = MovieCollection.readFromJson();
+
+        // это для домашки
+        // выберите любое количество заказов, какое вам нравится.
+
+        //var orders = RestaurantOrders.read("orders_100.json").getOrders();
+        //var orders = RestaurantOrders.read("orders_1000.json").getOrders();
+        //var orders = RestaurantOrders.read("orders_10_000.json").getOrders();
+
+        // протестировать ваши методы вы можете как раз в этом файле (или в любом другом, в котором вам будет удобно)
+    }
+}
